9dd24c64b3d733342259a2b9c6475b059969d0ac
Vitaly-IS-23 <vitalie01rus01@gmail.com>
2024-11-16 16:09:29 +0300

    Выполнены задачи 1-3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задача 1
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firstName + " " + middleName + " " + lastName;
+
+        System.out.print("Ф. И. О. сотрудника — " + fullName);
+
+        //Задча 2
+        System.out.print("\n\nДанные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+        //Задача 3
+        fullName = "Иванов Семён Семёнович";
+
+        System.out.println("\n\nДанные Ф. И. О. сотрудника — " + fullName.replace("ё", "е").replace("Ё", "Е"));
     }
 }
